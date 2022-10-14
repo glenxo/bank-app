eb922f404c329285dad752b0e33b0f3fda07cab1
¨glenxo¨ <glndxn80@gmail.com>
2022-10-14 03:12:59 -0500

    added components & structure

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import navbar from '/components/navbar';
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={'${styles.paddingX} ${styles.flexCenter}'}>
+      <div className={'${styles.boxWidth}'}>
+        navbar
+      </div>
+    </div>
+
+    <div className={'bg-primary ${styles.flexStart}'}>
+      <div className={'${styles.boxWidth}'}>
+        Hero
+      </div>
+    </div>
+
+    <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
+      <div className={'${styles.boxWidth}'}>
+        Stats
+        Business
+        billing
+        CardDeal 
+        Testimonials
+        clients
+        CTA
+        Footer
+      </div>
+    </div>
+
+  </div>
+)
+
 
 export default App
