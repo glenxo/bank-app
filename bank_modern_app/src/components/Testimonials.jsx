82f09a0556882425b2278e8949c8d502bbfc1222
glenxo <glndxn80@gmail.com>
2022-10-19 11:56:18 -0500

    added testimonials

--- bank_modern_app/src/components/Testimonials.jsx
+++ bank_modern_app/src/components/Testimonials.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-export default Testimonials
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={styles.heading2}>
+        Lo que la gente <br className="sm:block hidden" /> habla de nosotros  
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+        Todo lo que necesitas para aceptar pagos con tu tarjeta y hacer crecer tu negocio
+        en cualquier parte del mundo.
+        </p>
+      </div>
+    </div>
+
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
+    </div>
+  </section>
+);
+
+export default Testimonials;
